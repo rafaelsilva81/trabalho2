12b53f5a3a23bdd7d7f3093ec5c290ae5d66932e
rafaelsilva81 <rafaelgaldinosilva81@gmail.com>
2023-06-28 21:07:23 -0300

    fixing positions

--- main.cpp
+++ main.cpp
@@ -44,21 +44,12 @@ Parede *paredeLeft = new Parede(1, -5., 0., 0., 0., 270., 0., 1., 1., 1., false,
 Parede *paredeBack = new Parede(1, 0., 0., -5., 0., 180., 0., 1., 1., 1., false, false);
 
 int current_object_id = 0;
-int max_object_id = 0;
+int max_object_id = -1;
 
 vector<Objeto *> objetos;
 
 // Indica se o usuário está no "modo de seleção" de objetos
 bool selecting_state = false;
-
-// Indica se o usuário pediu pra salvar a cena
-bool asked_to_save = false;
-
-// Indica se o usuário esta no modo de transformação
-bool transforming_state = false;
-
-// Indica se o usuário esta no modo de movimentação de luz
-bool moving_light_state = false;
 
 // Indica a camera
 int camera_presets = 0;
@@ -85,8 +76,8 @@ void transformObjects()
   {
     // Translações
     objetos[pontoSelecionado - 1]->trans_x += 10 * glutGUI::dtx;
-    objetos[pontoSelecionado - 1]->trans_y += 2 * glutGUI::dty;
-    objetos[pontoSelecionado - 1]->trans_z += 2 * glutGUI::dtz;
+    objetos[pontoSelecionado - 1]->trans_y += 10 * glutGUI::dty;
+    objetos[pontoSelecionado - 1]->trans_z += 10 * glutGUI::dtz;
 
     // Rotações
     objetos[pontoSelecionado - 1]->rot_x += 10 * glutGUI::dax;
@@ -98,31 +89,12 @@ void transformObjects()
     objetos[pontoSelecionado - 1]->scale_y += 10 * glutGUI::dsy;
     objetos[pontoSelecionado - 1]->scale_z += 10 * glutGUI::dsz;
   }
-  /*
-    // Aplicando transformações no objeto selecionado
-    if (selecting_state)
-    {
-      // Translações
-      objetos[current_object_id]->trans_x += 10 * glutGUI::dtx;
-      objetos[current_object_id]->trans_y += 10 * glutGUI::dty;
-      objetos[current_object_id]->trans_z += 10 * glutGUI::dtz;
-
-      // Rotações
-      objetos[current_object_id]->rot_x += 10 * glutGUI::dax;
-      objetos[current_object_id]->rot_y += 10 * glutGUI::day;
-      objetos[current_object_id]->rot_z += 10 * glutGUI::daz;
-
-      // Escalas
-      objetos[current_object_id]->scale_x += 10 * glutGUI::dsx;
-      objetos[current_object_id]->scale_y += 10 * glutGUI::dsy;
-      objetos[current_object_id]->scale_z += 10 * glutGUI::dsz;
-    } */
 }
 
 int giveId()
 {
   /*  cout << "Atribuindo id:" + to_string(max_object_id) << endl; */
-  max_object_id += 1;
+  max_object_id = +1;
   return max_object_id;
 }
 
@@ -134,28 +106,6 @@ void saveScene()
 
   // Limpar o arquivo (precaução)
   f.clear();
-
-  /*
-
-    Mesa *mesa = new Mesa(1, 3.0, 0.0, 2.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, false);
-    FORMATO DO JSON
-    [
-      {
-        id: 1,
-        class: "Mesa",
-        t_x: 3.0,
-        t_y: 0.0,
-        t_z: 2.0,
-        r_x: 0.0,
-        r_y: 0.0,
-        r_z: 0.0,
-        s_x: 1.0,
-        s_y: 1.0,
-        s_z: 1.0,
-        selected: false
-      },
-    ]
-  */
 
   // Salvar os objetos
   json j;
@@ -186,8 +136,6 @@ void saveScene()
   // Salvar o json no arquivo
   std::ofstream o("save.json");
   o << std::setw(4) << j << std::endl; // 4 espaços de indentação
-
-  asked_to_save = true;
 }
 
 void readSave()
@@ -210,20 +158,17 @@ void readSave()
      */
 
     // Balcao, armario e prateleira no fundo
-    Balcao *balcao = new Balcao(giveId(), -2.0, 0.0, -2.5, 0., 360., 0., 1., 1., 1., false, false);
+    Balcao *balcao = new Balcao(giveId(), -0.8, 0.0, -3, 0., 360., 0., 1., 1., 1., false, false);
 
-    Armario *armario_fundo = new Armario(giveId(), -4.0, 0.0, -4.5, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, false, false);
-    Barril *barril_armario_fundo_1 = new Barril(giveId(), -4.0, 0.1, -4.5, 0., 0., 0., 1., 1., 1., false, false);
+    Armario *armario_fundo = new Armario(giveId(), -3.0, 0.0, -4.4, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, false, false);
+    Barril *barril_armario_fundo_1 = new Barril(giveId(), -3.0, 0.1, -4.4, 0., 0., 0., 1., 1., 1., false, false);
 
-    Prateleira *prateleira_fundo = new Prateleira(giveId(), -2., 2.2, -4.7, 0., 0., 0., 1., 1., 1., false, false);
-    Cobertura *cobertura_prateleira_fundo = new Cobertura(giveId(), -2., 1.85, -5, 0., 0., 0., 1., 1., 1., false, false);
-    Caneca *caneca_prateleira_fundo_1 = new Caneca(giveId(), -1.5, 2.4, -4.7, 0., 180., 0., 1., 1., 1., false, false);
-    Caneca *caneca_prateleira_fundo_2 = new Caneca(giveId(), -1.8, 2.4, -4.7, 0., 180., 0., 1., 1., 1., false, false);
+    Prateleira *prateleira_fundo = new Prateleira(giveId(), -1., 2.2, -4.6, 0., 0., 0., 1., 1., 1., false, false);
+    Cobertura *cobertura_prateleira_fundo = new Cobertura(giveId(), -1., 1.75, -5, 0., 0., 0., 1., 1., 1., false, false);
+    Caneca *caneca_prateleira_fundo_1 = new Caneca(giveId(), -0.5, 2.4, -4.6, 0., 180., 0., 1., 1., 1., false, false);
 
-    Tamborete *tamborete_balcao_1 = new Tamborete(giveId(), -4.2, 0.0, -1.9, 0., 0., 0., 1., 1., 1., false, false);
-
-    Caneca *caneca_balcao_1 = new Caneca(giveId(), -4.3, 1, -2.4, 0., 10, 0., 1., 1., 1., false, false);
-    Caneca *caneca_balcao_2 = new Caneca(giveId(), -3.3, 1, -2.4, 0., -20, 0., 1., 1., 1., false, false);
+    Tamborete *tamborete_balcao_1 = new Tamborete(giveId(), -3.1, 0.0, -2.2, 0., 0., 0., 1., 1., 1., false, false);
+    Caneca *caneca_balcao_1 = new Caneca(giveId(), -3.1, 1, -3, 0., 10, 0., 1., 1., 1., false, false);
 
     Mesa *mesa_1 = new Mesa(giveId(), 3.0, 0.0, 2.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, false, false);
     Tamborete *tamborete_1_mesa_1 = new Tamborete(giveId(), 2.3, 0.0, 2.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, false, false);
@@ -235,8 +180,8 @@ void readSave()
     Tamborete *tamborete_1_mesa_2 = new Tamborete(giveId(), -3.6, 0.0, 0.5, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, false, false);
     Caneca *caneca_1_mesa_2 = new Caneca(giveId(), -3.1, 1, 0.6, 0.0, -84.0, 0.0, 1.0, 1.0, 1.0, false, false);
 
-    Palco *palco = new Palco(giveId(), 3.5, 0.0, -4.5, 0.0, 0.0, 0.0, 0.9, 0.9, 0.9, false, false);
-    Microfone *microfone = new Microfone(giveId(), 3, 0.85, -3.8, 0.0, 50.0, 0.0, 0.9, 0.9, 0.9, false, false);
+    Palco *palco = new Palco(giveId(), 4, 0.0, -4.5, 0.0, 0.0, 0.0, 0.6, 0.9, 0.9, false, false);
+    Microfone *microfone = new Microfone(giveId(), 3.5, 0.85, -3.8, 0.0, 50.0, 0.0, 0.6, 0.9, 0.9, false, false);
 
     objetos.push_back(balcao);
 
@@ -246,12 +191,10 @@ void readSave()
     objetos.push_back(prateleira_fundo);
     objetos.push_back(cobertura_prateleira_fundo);
     objetos.push_back(caneca_prateleira_fundo_1);
-    objetos.push_back(caneca_prateleira_fundo_2);
 
     objetos.push_back(tamborete_balcao_1);
 
     objetos.push_back(caneca_balcao_1);
-    objetos.push_back(caneca_balcao_2);
 
     objetos.push_back(mesa_1);
     objetos.push_back(tamborete_1_mesa_1);
@@ -326,66 +269,10 @@ void readSave()
       {
         objetos.push_back(new Microfone(id, t_x, t_y, t_z, r_x, r_y, r_z, s_x, s_y, s_z, false, false));
       }
-
-      // @TODO: Adicionar os novos objetos
     }
   }
 
   cout << "PROCESSO DE LEITURA DE SAVE CONCLUIDO" << endl;
-}
-
-void drawString(std::string str, int x, int y)
-{
-
-  /*
-    No OpenGL, existem dois modos de exibição principais: o modo de projeção e o modo de modelagem.
-
-    No caso da função em questão, estamos definindo o modo de projeção para desenhar um texto em uma
-    janela bidimensional usando a função gluOrtho2D(),
-    que define um sistema de coordenadas bidimensional de 800x600.
-
-    Em seguida, o texto é desenhado na tela usando as funções do GLUT glutBitmapCharacter() e glRasterPos2i()
-
-  */
-  // Define o modo de projeção para desenhar o texto na tela
-  glMatrixMode(GL_PROJECTION);
-
-  // Salva a matriz atual na pilha de matrizes OpenGL
-  glPushMatrix();
-
-  // Define a matriz de projeção como uma matriz de identidade para garantir que não haja transformações aplicadas a ela
-  glLoadIdentity();
-
-  // Define o sistema de coordenadas bidimensional com o canto inferior esquerdo em (0,0) e o canto superior direito em (800,600)
-  gluOrtho2D(0, 800, 0, 600);
-
-  // Define o modo de modelagem para desenhar o texto na tela
-  glMatrixMode(GL_MODELVIEW);
-
-  // Salva a matriz atual na pilha de matrizes OpenGL
-  glPushMatrix();
-
-  // Define a matriz de modelagem como uma matriz de identidade para garantir que não haja transformações aplicadas a ela
-  glLoadIdentity();
-
-  // Define a posição do ponto onde o texto será desenhado com as coordenadas (x,y)
-  glRasterPos2i(x, y);
-
-  // Percorre cada caractere da string e desenha na posição atual do raster utilizando uma fonte 9x15 pixels
-  for (char c : str)
-    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, c);
-
-  // Restaura a matriz de modelagem anteriormente salva na pilha de matrizes OpenGL
-  glPopMatrix();
-
-  // Define o modo de modelagem para desenhar outros objetos na cena
-  glMatrixMode(GL_MODELVIEW);
-
-  // Restaura a matriz de projeção anteriormente salva na pilha de matrizes OpenGL
-  glPopMatrix();
-
-  // Define o modo de projeção para desenhar outros objetos na cena
-  glMatrixMode(GL_PROJECTION);
 }
 
 void teclado(unsigned char tecla, int mouseX, int mouseY)
@@ -394,20 +281,23 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
 
   switch (tecla)
   {
-  case 't':
-    transforming_state = !transforming_state;
-    glutGUI::trans_obj = !glutGUI::trans_obj;
-    break;
-  case 'l':
-    moving_light_state = !moving_light_state;
-    glutGUI::trans_luz = !glutGUI::trans_luz;
-    break;
+  // ------------------------- TECLAS: sombra -------------------------
   case 's':
     // Sombra
     if (selecting_state)
     {
       drawShadow = !drawShadow;
     }
+    break;
+  case 't':
+    // mostra sombras em planos arbitrários(parede, chão, plano inclinado)
+    sombras_planos = !sombras_planos;
+    break;
+  case 'o':
+    // Troca entre luz pontual próxima e luz distante
+    tipo_luz = !tipo_luz;
+    break;
+  // ------------------------- TECLAS: legado -------------------------
   case 'p':
     objetos[current_object_id]->selected = false;
     // Alterar o modo de seleção
@@ -420,56 +310,17 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
       objetos[current_object_id]->selected = true;
     }
     break;
-  case 'n':
-    // Vai pro proximo objeto
-    objetos[current_object_id]->selected = false;
-    // Alterar o objeto atualmente selecionado
-    if (selecting_state)
-    {
-      // Alternar o current_id
-      if (current_object_id >= objetos.size() - 1)
-      {
-        current_object_id = 0;
-      }
-      else
-      {
-        current_object_id++;
-      }
-
-      // Atualizar o objeto selecionado
-      objetos[current_object_id]->selected = true;
-    }
+  case 'r':
+    glutGUI::trans_obj = !glutGUI::trans_obj;
     break;
-  case 'b':
-    // Volta pro objeto anterior
-    objetos[current_object_id]->selected = false;
-
-    // Alterar o objeto atualmente selecionado
-    if (selecting_state)
-    {
-      // Alternar o current_id
-      if (current_object_id <= 0)
-      {
-        current_object_id = objetos.size() - 1;
-      }
-      else
-      {
-        current_object_id--;
-      }
-
-      // Atualizar o objeto selecionado
-      objetos[current_object_id]
-          ->selected = true;
-    }
+  case 'l':
+    glutGUI::trans_luz = !glutGUI::trans_luz;
     break;
+  // ------------------------- TECLAS: viewports/persectiva -------------------------
   case 'v':
-    // Vetor de Coordenadas
-    if (selecting_state)
-    {
-      objetos[current_object_id]->show_coord = !objetos[current_object_id]->show_coord;
-    }
+    viewports = !viewports;
     break;
-  case 'u':
+  case '1':
     // Perpectiva: 1 ponto de fuga
     glutGUI::cam->e.x = 1.5;
     glutGUI::cam->e.y = -0.25;
@@ -493,7 +344,7 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 1;
     glutGUI::cam->u.z = 0;
     break;
-  case '0':
+  case '3':
     // Perpectiva: 3 pontos de fuga
     glutGUI::cam->e.x = 1.5;
     glutGUI::cam->e.y = -0.25;
@@ -505,7 +356,7 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 1;
     glutGUI::cam->u.z = 0;
     break;
-  case '1':
+  case '4':
     // Ortográfica(vista): Frontal
     glutGUI::cam->e.x = 0;
     glutGUI::cam->e.y = 0.5;
@@ -517,7 +368,7 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 1;
     glutGUI::cam->u.z = 0;
     break;
-  case '%':
+  case '5':
     // Ortográfica(vista): Lateral
     glutGUI::cam->e.x = 2;
     glutGUI::cam->e.y = 0.5;
@@ -529,7 +380,7 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 1;
     glutGUI::cam->u.z = 0;
     break;
-  case '&':
+  case '6':
     // Ortográfica(vista): Planta
     glutGUI::cam->e.x = 0;
     glutGUI::cam->e.y = 4;
@@ -541,7 +392,7 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 0;
     glutGUI::cam->u.z = -1;
     break;
-  case '*':
+  case '7':
     // Ortográfica(axonométrica): Isométrica
     glutGUI::cam->e.x = 3;
     glutGUI::cam->e.y = 2;
@@ -553,7 +404,7 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 1;
     glutGUI::cam->u.z = 0;
     break;
-  case '(':
+  case '8':
     // Ortográfica(axonométrica): Dimétrica
     glutGUI::cam->e.x = 3;
     glutGUI::cam->e.y = 1;
@@ -565,7 +416,7 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 1;
     glutGUI::cam->u.z = 0;
     break;
-  case ')':
+  case '9':
     // Ortográfica(axonométrica): Trimétrica
     glutGUI::cam->e.x = 3;
     glutGUI::cam->e.y = 1;
@@ -577,9 +428,11 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
     glutGUI::cam->u.y = 1;
     glutGUI::cam->u.z = 0;
     break;
-  case 'j':
-    viewports = !viewports;
+  case 'i':
+    // Isométrica
+    glutGUI::perspective = !glutGUI::perspective;
     break;
+
   default:
     break;
   }
@@ -587,6 +440,9 @@ void teclado(unsigned char tecla, int mouseX, int mouseY)
 
 void montarCena()
 {
+
+  glClearColor(0.53, 0.81, 0.92, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT);
 
   // GUI::setLight(0,  3,5,4, true,false);//(tecla de apagar, x,y,z , desligar e ligar luz, (false = forte, true = atenuada))
 
@@ -596,7 +452,8 @@ void montarCena()
 
   glPushMatrix();
   //-------------------sombra-------------------
-  glTranslated(0.0, k, 0.0);        // glTranslated(0.0,k-0.001,0.0);
+  glTranslated(0.0, k, 0.0); // glTranslated(0.0,k-0.001,0.0);
+  GUI::setColor(0.8, 0.8, 0.8, 1, true);
   GUI::drawFloor(10, 10, 0.1, 0.1); //(largura, comprimento, vertices largura, vertices comprimento)
 
   // GUI::drawPlane(Vetor3D(2,2,3), k, 15, 15, 0.5, 0.5); //chama o drawFloor dentro //-0.001 definido dentro do drawFloor
@@ -779,73 +636,12 @@ void desenha()
   GUI::displayEnd();
 }
 
-// =============== PICKING ================
-// TODO: alterar tudo aqui (nomes, vars)
-// TODO:Fix picking
-void desenhaPontosDeControle()
-{
-  cout << "func desenhaPontosDeControle" << endl;
-  for (size_t i = 0; i < objetos.size(); i++)
-  {
-    glPushName(i + 1);
-    objetos[i]->desenha();
-    glPopName();
-  }
-}
-
-int picking(GLint cursorX, GLint cursorY, int w, int h)
-{
-
-  int BUFSIZE = 512;
-  GLuint selectBuf[512];
-  GUI::pickingInit(cursorX, cursorY, w, h, selectBuf, BUFSIZE);
-  GUI::displayInit();
-  objetos[current_object_id]->selected = false;
-  desenhaPontosDeControle();
-  return GUI::pickingClosestName(selectBuf, BUFSIZE);
-}
-
-void mouse(int button, int state, int x, int y)
-{
-  GUI::mouseButtonInit(button, state, x, y);
-
-  // if the left button is pressed
-  if (button == GLUT_LEFT_BUTTON)
-  {
-    cout << "x: " << x << " y: " << y << endl;
-    // when the button is pressed
-    if (state == GLUT_DOWN)
-    {
-      cout << "down" << endl;
-      // picking
-      int pick = picking(x, y, 5, 5);
-      if (pick != 0)
-      {
-        cout << "pick: " << pick << endl;
-        selecting_state = true;
-        pontoSelecionado = pick;
-
-        cout << "ponto selecionado: " << pontoSelecionado << endl;
-        objetos[pontoSelecionado - 1]->selected = true;
-        current_object_id = pontoSelecionado - 1;
-        glutGUI::lbpressed = false;
-      }
-      else
-      {
-        cout << "pick: " << pick << endl;
-        cout << "nada selecionado" << endl;
-        selecting_state = false;
-      }
-    }
-  }
-}
-
 int main(int argc, char *argv[])
 {
 
   readSave();
   // GUI gui(800,600); // (largura, altura)
-  GUI gui = GUI(800, 600, desenha, teclado, mouse);
+  GUI gui = GUI(800, 600, desenha, teclado);
   // TODO: GUI gui = GUI(800, 600, desenha, teclado, mouse);
   // GUI gui2 = GUI(500, 200, desenha2);
 }
